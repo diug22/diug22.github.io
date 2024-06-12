127cbc6d51e2c4f2d5111d824022ca10272a5c8d
Joaquin Brotons Candela <brotons22@gmail.com>
2024-06-12 12:14:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function uploadAudio() {
     var formData = new FormData();
     formData.append('audio', file);
 
-    fetch('https://139.47.125.37:5000/generate', {
+    fetch('https://eb43-139-47-125-37.ngrok-free.app/generate', {
         method: 'POST',
         body: formData,
         headers: {
