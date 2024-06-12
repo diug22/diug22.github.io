b2b7804b9025992f30576323b1111c9c09988c49
Joaquin Brotons Candela <brotons22@gmail.com>
2024-06-12 11:16:17 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function uploadAudio() {
+    var fileInput = document.getElementById('audioInput');
+    var file = fileInput.files[0];
+    var formData = new FormData();
+    formData.append('audio', file);
+
+    fetch('http://139.47.125.37:5000/generate', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'X-API-KEY': 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'  // Asegúrate de cambiar esto por tu clave API real
+        }
+    })
+    .then(response => response.blob())
+    .then(blob => {
+        var url = URL.createObjectURL(blob);
+        var video = document.getElementById('resultVideo');
+        video.src = url;
+        video.load();
+        video.play();
+    })
+    .catch(error => console.error('Error al subir el archivo:', error));
+}
